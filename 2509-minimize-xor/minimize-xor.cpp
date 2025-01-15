0f83040a1f49e85cb758c0726fcfaa96246ebc17
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-01-15 11:39:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (10.75%) - LeetSync

--- 2509-minimize-xor/minimize-xor.cpp
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -12,34 +12,61 @@ public:
     }
     int minimizeXor(int num1, int num2) {
              
-             int  x=num1;
-             int req= __builtin_popcount(num2);
-             int curr=__builtin_popcount(x);
+       int x=0;
+       int req=__builtin_popcount(num2);
 
-             int bit=0;
+       for (int bit=31;bit>=0 && req>0 ;bit--){
             
-            if (curr<req){
-                while (curr<req){
-                    if (!isset(x,bit)){
-                        setbit(x,bit);
-                        curr++;
-                    }
-                    bit++;
-
-                }
+            if (isset(num1,bit)){
+                setbit(x,bit);
+                req--;
             }
-           else if (curr>req){
-                while (curr>req){
-                    if (isset(x,bit)){
-                        unsetbit(x,bit);
-                        curr--;
-                    }
-                    bit++;
-
-                }
+       }
+       for (int bit=0;bit<=31 && req>0 ;bit++){
+            
+            if (!isset(num1,bit)){
+                setbit(x,bit);
+                req--;
             }
+       }
+       
+       return x;
+       
+       
+       
+       
+       
+       
+       
+       
+        //      int  x=num1;
+        //      int req= __builtin_popcount(num2);
+        //      int curr=__builtin_popcount(x);
 
-            return x;
+        //      int bit=0;
+            
+        //     if (curr<req){
+        //         while (curr<req){
+        //             if (!isset(x,bit)){
+        //                 setbit(x,bit);
+        //                 curr++;
+        //             }
+        //             bit++;
+
+        //         }
+        //     }
+        //    else if (curr>req){
+        //         while (curr>req){
+        //             if (isset(x,bit)){
+        //                 unsetbit(x,bit);
+        //                 curr--;
+        //             }
+        //             bit++;
+
+        //         }
+        //     }
+
+        //     return x;
 
 
 
