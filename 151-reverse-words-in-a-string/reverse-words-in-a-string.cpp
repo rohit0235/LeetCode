5e630a9de3586d6a2d22cd6a03565c235d67686a
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-01-19 19:45:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (63.86%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -2,21 +2,49 @@ class Solution {
 public:
     string reverseWords(string s) {
         int n=s.size();
-           stringstream ss(s);
-
-           string token="";
-           string result="";
-
-           while (ss>>token){
-              
-             if (!result.empty()) {
-            result = token + " " + result; 
-        } else {
-            result = token; 
+                  reverse(s.begin(),s.end()); 
+        int i=0;
+         int j=0;
+        while (s[i]==' '){
+            i++;
+            j++;
         }
-           }
 
-           return result;
+         string result="";
+        while (j<n){
+             string temp="";
+          while  (j<n && s[j]!=' '){
+                  temp+=s[j];
+                   j++;
+          }    
+          reverse(temp.begin(),temp.end());
+          if (!temp.empty())
+          result+=temp+" ";
+          j++;
+            
+        }
+        return result.substr(0,result.size()-1);
+
+
+
+
+
+
+        //    stringstream ss(s);
+
+        //    string token="";
+        //    string result="";
+
+        //    while (ss>>token){
+              
+        //             if (!result.empty()) {
+        //             result = token + " " + result; 
+        //         } else {
+        //             result = token; 
+        //         }
+        //    }
+
+        //    return result;
 
                 
     }
