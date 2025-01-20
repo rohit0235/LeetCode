e0e4d6dade131bd1b4d4af08171bc84853b41b5e
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-01-20 22:38:09 +0530

    Time: 331 ms (11.25%) | Memory: 35.9 MB (28.73%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+            
+            int total=accumulate(nums.begin(),nums.end(),0);
+            int n =nums.size();
+               int leftsum=0;
+               
+            for (int i=0;i<n;i++){
+                 
+                  leftsum+=nums[i];
+                  cout<<leftsum<<" "<<total-leftsum<<" "<<endl;
+                  if (leftsum-nums[i]==total-leftsum){
+                    return i;
+                  }
+                  
+            }
+            return -1;
+    }
+};
