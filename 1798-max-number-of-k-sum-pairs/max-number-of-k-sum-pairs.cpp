5b70a26abb4a99196ebd9024f7c8a32cc729dd69
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-01-21 21:38:05 +0530

    Time: 92 ms (60.69%) | Memory: 61.7 MB (99.82%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -1,24 +1,55 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        
-        unordered_map<int,int>mp;
-       int maxo=0;
-        for (int i:nums){
-                if (i>=k) continue;
-                if (mp.find(abs(i-k))!=mp.end()){
-                    cout<<i<<" "<<k<<endl;
-                     maxo++;
-                      mp[abs(i-k)]--;
-                      if (mp[abs(i-k)]<=0){
-                        mp.erase(abs(i-k));
-                      }
-                }
-                else{
-                    mp[i]++;
-                }
-        }
-        return maxo;
+       
+       sort(nums.begin(),nums.end());
+         int n=nums.size();
+       int oper=0;
+       int i=0;
+       int j=n-1;
+       while (i<j){
+           int sum=nums[i]+nums[j];
+           if (sum==k) {
+            oper++;
+            i++;
+            j--;
+           }
+
+           else if (sum<k){
+                 i++;
+           }
+
+           else{
+                j--;
+           }
+           
+       }
+       
+       return oper++;
+
+
+
+
+
+
+
+    //     unordered_map<int,int>mp;
+    //    int maxo=0;
+    //     for (int i:nums){
+    //             if (i>=k) continue;
+    //             if (mp.find(abs(i-k))!=mp.end()){
+    //                 cout<<i<<" "<<k<<endl;
+    //                  maxo++;
+    //                   mp[abs(i-k)]--;
+    //                   if (mp[abs(i-k)]<=0){
+    //                     mp.erase(abs(i-k));
+    //                   }
+    //             }
+    //             else{
+    //                 mp[i]++;
+    //             }
+    //     }
+    //     return maxo;
 
 
 
