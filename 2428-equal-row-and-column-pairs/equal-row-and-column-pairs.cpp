99aa9c4536d3bb149b33dcb3aa4c29f59db9bc96
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-01-22 18:50:52 +0530

    Time: 23 ms (54.41%) | Memory: 39.3 MB (35.54%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-struct VectorHash {
-    size_t operator()(const std::vector<int>& v) const {
-        std::hash<int> hasher;
-        size_t seed = 0;
-        for (int i : v) {
-            seed ^= hasher(i) + 0x9e3779b9 + (seed << 6) + (seed >> 2); // Combine hashes
-        }
-        return seed;
-    }
-};
+// struct VectorHash {
+//     size_t operator()(const std::vector<int>& v) const {
+//         std::hash<int> hasher;
+//         size_t seed = 0;
+//         for (int i : v) {
+//             seed ^= hasher(i) + 0x9e3779b9 + (seed << 6) + (seed >> 2); // Combine hashes
+//         }
+//         return seed;
+//     }
+// };
     int equalPairs(vector<vector<int>>& grid) {
         
         int count=0;
-        unordered_map<std::vector<int>, int, VectorHash> mp;
+           map<std::vector<int>, int> mp;
         for (auto i:grid) mp[i]++; 
 
         for (int i=0;i<grid[0].size();i++){
