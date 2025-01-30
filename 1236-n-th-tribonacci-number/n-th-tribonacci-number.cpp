a0f35653bc55880420b9180c813c2157377cd026
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-01-30 23:57:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (32.86%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -2,21 +2,37 @@ class Solution {
 public:
     int tribonacci(int n) {
           
-       int t0=0;
-       int t1=1;
-       int t2=1;
-       if (n==0) return t0;
-       if (n==1) return t1;
-       if (n==2) return t2;
+          vector<int>dp(n+1,0);
+          if (n==0) return 0;
+          if (n==1) return 1;
+          if (n==3) return 2;
+          
+          dp[0]=0;
+          dp[1]=1;
+          dp[2]=1;
+          dp[n-1]=-1;
+          for (int i=3;i<=n;i++){
+               dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+          }
+          
+          return dp[n];
 
-       for (int i=2;i<n;i++){
-              int sum=t0+t1+t2;
-              t0=t1;
-              t1=t2;
-              t2=sum;
-       }
 
-       return t2;
+    //    int t0=0;
+    //    int t1=1;
+    //    int t2=1;
+    //    if (n==0) return t0;
+    //    if (n==1) return t1;
+    //    if (n==2) return t2;
+
+    //    for (int i=2;i<n;i++){
+    //           int sum=t0+t1+t2;
+    //           t0=t1;
+    //           t1=t2;
+    //           t2=sum;
+    //    }
+
+    //    return t2;
 
         
     }
