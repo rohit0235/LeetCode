db7fb9c60a40d03d708674f238c99c7b413f68fc
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-01-30 23:47:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (65.58%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+          
+       int t0=0;
+       int t1=1;
+       int t2=1;
+       if (n==0) return t0;
+       if (n==1) return t1;
+       if (n==2) return t2;
+
+       for (int i=2;i<n;i++){
+              int sum=t0+t1+t2;
+              t0=t1;
+              t1=t2;
+              t2=sum;
+       }
+
+       return t2;
+
+        
+    }
+};
