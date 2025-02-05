d0cedf01e45fcce469ec85947e99ee891f1628ed
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-02-05 08:17:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (61.87%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
@@ -3,21 +3,25 @@ public:
     bool areAlmostEqual(string s1, string s2) {
            if (s1==s2) return true;
             int n =s1.size();
-            int count=0;
-            for (int i=0;i<n;i++) if (s1[i]!=s2[i]) count++;
-             if (count>2) return false;
+            // int count=0;
+            // for (int i=0;i<n;i++) if (s1[i]!=s2[i]) count++;
+            //  if (count>2) return false;
             char checkf='\0';
             char checks='\0';
+            int count=0;
            for (int i=0;i<n;i++){
                    
                    if (checkf=='\0' && s1[i]!=s2[i]){
+                           if (count>0) return false;
                            checkf=s1[i];
                            checks=s2[i];
                    }
                    else if (s1[i]!=s2[i]){
 
                            if (s1[i]==checks && s2[i]==checkf){
-                                return true;
+                            count++;
+                            checkf='\0';
+                            
                            }         
                            else{
                             return false;
