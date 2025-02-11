29f79a7bd2aa855f2e65ce3892bef792e95ddd9a
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-02-11 08:51:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (68.15%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+         
+          while(s.find(part)!=string::npos){
+                 int index=s.find(part);
+
+                 s.erase(index,part.size());
+          }
+
+          return s;
+    }
+};
