ddb50218fc36335e69b9d56fcba69dcf80ba2a29
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-02-17 12:21:11 +0530

    Time: 3 ms (78.62%) | Memory: 7.7 MB (97.61%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
@@ -1,34 +1,54 @@
 class Solution {
 public:
-      int n;
-     void solve(string &curr, unordered_set<string>&result,vector<bool>&used,string tiles){
+    //   int n;
+    //  void solve(string &curr, unordered_set<string>&result,vector<bool>&used,string tiles){
            
-            result.insert(curr);
+    //         result.insert(curr);
 
        
 
-           for (int i=0;i<n;i++){
-                    if (used[i]) continue;
-                    used[i]=true;
-                 curr.push_back(tiles[i]);
-                 solve(curr,result,used,tiles);
+    //        for (int i=0;i<n;i++){
+    //                 if (used[i]) continue;
+    //                 used[i]=true;
+    //              curr.push_back(tiles[i]);
+    //              solve(curr,result,used,tiles);
                  
-                  used[i]=false;
-                 curr.pop_back();
+    //               used[i]=false;
+    //              curr.pop_back();
+    //        }
+            
+    //  }
+     int count;
+    void solve (vector<int>&vec){
+           
+           count++;
+
+           for (int i=0;i<26;i++){
+                   
+                   if (vec[i]==0) continue;
+                   
+                   vec[i]--;
+                   solve(vec);
+                   vec[i]++;
            }
-            
-     }
+    }
     int numTilePossibilities(string tiles) {
-             n =tiles.size();
+        //      n =tiles.size();
 
-           vector<bool>used(n,false);
+        //    vector<bool>used(n,false);
 
-           unordered_set<string>result;
-           string curr="";
-           solve (curr,result,used,tiles);
+        //    unordered_set<string>result;
+        //    string curr="";
+        //    solve (curr,result,used,tiles);
 
-           return result.size()-1;
+        //    return result.size()-1;
             
+           count=0;
+             vector<int>vec(26,0);
 
+             for (char i:tiles) vec[i-'A']++;
+            solve (vec);
+
+            return count-1;
     }
 };
