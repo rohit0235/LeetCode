a434ec4a267eaac647afd217c88b3f000e815c71
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-02-23 15:58:12 +0530

    Time: 1 ms (51.99%) | Memory: 28.5 MB (32.25%) - LeetSync

--- 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
 public:
-
+     unordered_map<int,int>mp;
      TreeNode *solve(int prestart,int poststart,int preend,vector<int>& preorder, vector<int>& postorder){
               
               if (prestart>preend) return NULL;
@@ -21,11 +21,11 @@ public:
                if (prestart==preend) return root;
                int next=preorder[prestart+1];
 
-               int j=poststart;
+               int j=mp[next];
 
-               while (postorder[j]!=next){
-                    j++;
-               }
+            //    while (postorder[j]!=next){
+            //         j++;
+            //    }
 
                int num=j-poststart+1;
 root->left=solve(prestart+1,poststart,prestart+num,preorder,postorder);
@@ -38,7 +38,9 @@ root->right=solve(prestart+num+1,j+1,preend,preorder,postorder);
     TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
             
             int n=preorder.size();
-
+            for (int i=0;i<postorder.size();i++) {
+                 mp[postorder[i]]=i;
+            }
             return  solve(0,0,n-1,preorder,postorder);
 
 
