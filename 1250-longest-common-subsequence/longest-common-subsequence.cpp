9a1adffc18950c882f7851b8c6ddae59bf9e5426
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-02-28 19:55:32 +0530

    Time: 27 ms (56.57%) | Memory: 27.3 MB (74.95%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -17,7 +17,25 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
-        vector<vector<int>>memo(n+1,vector<int>(m+1,-1));
-        return solve(text1,text2,n,m,memo);
+        // vector<vector<int>>memo(n+1,vector<int>(m+1,-1));
+        vector<vector<int>>memo(n+1,vector<int>(m+1,0));
+        // return solve(text1,text2,n,m,memo);
+          
+        for(int i=1;i<=n;i++){
+                
+                for(int j=1;j<=m;j++){
+                     
+                     if (text1[i-1]==text2[j-1]){
+                          
+                          memo[i][j]=1+memo[i-1][j-1];
+                     }
+                     else{
+                          memo[i][j]=max(memo[i][j-1],memo[i-1][j]);
+                     }
+                }
+        }
+
+        return memo[n][m];
+
     }
 };
