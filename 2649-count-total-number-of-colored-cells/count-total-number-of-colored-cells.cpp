82942bf04ef51000da310f6a7c81e84d32293967
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-03-05 09:38:56 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (91.36%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-         if (n==1) return 1;
-         long long start=0;
-         long long ans=1;
-         for (int i=0;i<n-1;i++){
+     unsigned long long _n = n;
+        return 1 + 2 * (_n-1) * (_n);
+        //  if (n==1) return 1;
+        //  long long start=0;
+        //  long long ans=1;
+        //  for (int i=0;i<n-1;i++){
                
-               start+=4;
-               ans+=start;
+        //        start+=4;
+        //        ans+=start;
 
-         }
+        //  }
 
-         return ans;
+        //  return ans;
     }
 };
