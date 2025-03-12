17820118e4c0e2a8dc0ca99a24757ea839e874af
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-03-12 16:33:48 +0530

    Time: 0 ms (100.00%) | Memory: 21.3 MB (75.85%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -3,14 +3,67 @@ public:
     int maximumCount(vector<int>& nums) {
             int count=0;
             int n=nums.size();
-            int z=0;
-            for(int i:nums){
-                     if (i==0) z++;
-                     else if (i>0) break;
-                     else  count++;
-            }
 
-            return max(count,n-count-z);
+        // -3 -2 -1
+             if (nums[0]==0 && n==1) return 0; 
+             int left=0;
+             int right=n-1;
+           
+             while (left<n && left<=right ){
+                   int mid=left+(right-left)/2;
+                   if (nums[mid]<=0){
+                           left=mid+1;
+                   }
+                   else if (nums[left]<=0){
+                                    left++;
+                   }
+                   else if (nums[left]>0){
+                                  break;
+                   }
+             }
+
+             int   pos=right-left+1;
+            left=0;
+            right=n-1;
+                            
+
+           
+             while (right>0 && left<=right ){
+                   int mid=left+(right-left)/2;
+                   if (nums[mid]>=0){
+                     right=mid-1;
+                   }
+                  else if (nums[right]>=0){
+                      right--;
+                   }
+                   else if (nums[right]<0){
+                           break;
+                   }
+             }
+
+             int neg=right-left+1;
+                        // cout<<pos<<" "<<neg;
+             return max(pos,neg);
+
+            
+
+
+
+
+
+
+
+
+
+
+            // int z=0;
+            // for(int i:nums){
+            //          if (i==0) z++;
+            //          else if (i>0) break;
+            //          else  count++;
+            // }
+
+            // return max(count,n-count-z);
             
      }
 };
