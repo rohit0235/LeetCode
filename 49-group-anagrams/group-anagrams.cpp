08e412539075e2770d555100c7379be97e291b9d
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-03-16 02:36:46 +0530

    Time: 79 ms (6.70%) | Memory: 37.4 MB (5.31%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,35 +1,59 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-           
-               vector<vector<string>> res;
-               unordered_map<string,int>mp;
-                int n=strs.size();
-                int j=0;
-               for(int i=0;i<n;i++){
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {                     unordered_map<string,vector<string>>result;
+                 int n=strs.size();
+           for(string i:strs){
+                 
+                   vector<int>chars(26,0);
+
+                   for(char s:i){
+                        chars[s-'a']++;
+                   }
+                  string key="";
+                   for(int i=0;i<26 ;i++){
+                        key+=to_string(chars[i])+"#";
+                   }
+                result[key].push_back(i);
+           }
+            vector<vector<string>>res;
+           for(auto i:result){
+                 res.push_back(i.second);
+           }
+
+           return res;
+
+
+
+
+
+            //    vector<vector<string>> res;
+            //    unordered_map<string,int>mp;
+            //     int n=strs.size();
+            //     int j=0;
+            //    for(int i=0;i<n;i++){
                     
-                     string fin=strs[i];
+            //          string fin=strs[i];
 
-                     sort(strs[i].begin(),strs[i].end());
-                    //  for(auto i:mp){
-                    //      cout<<i.first<<" "<<i.second<<" ";
+            //          sort(strs[i].begin(),strs[i].end());
+            //         //  for(auto i:mp){
+            //         //      cout<<i.first<<" "<<i.second<<" ";
 
-                    //  }
-                    //  cout<<endl;
-                     if (mp.find(strs[i])!=mp.end()){
-                        int index=mp[strs[i]];
-                        //   cout<<index<<" "<<endl;
-                          res[index].push_back(fin);
-                     }
-                     else{
-                          res.push_back({fin}); 
-                           mp[strs[i]]=j;
-                           j++;
-                     }
+            //         //  }
+            //         //  cout<<endl;
+            //          if (mp.find(strs[i])!=mp.end()){
+            //             int index=mp[strs[i]];
+            //             //   cout<<index<<" "<<endl;
+            //               res[index].push_back(fin);
+            //          }
+            //          else{
+            //               res.push_back({fin}); 
+            //                mp[strs[i]]=j;
+            //                j++;
+            //          }
 
                    
-               }
+            //    }
 
-               return res;
+            //    return res;
     }
 };
