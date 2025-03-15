cfd1563ff1c8750346039498c1383861a3adc70b
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-03-16 02:10:40 +0530

    Time: 27 ms (24.64%) | Memory: 23.4 MB (96.81%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+           
+               vector<vector<string>> res;
+               unordered_map<string,int>mp;
+                int n=strs.size();
+                int j=0;
+               for(int i=0;i<n;i++){
+                    
+                     string fin=strs[i];
+
+                     sort(strs[i].begin(),strs[i].end());
+                    //  for(auto i:mp){
+                    //      cout<<i.first<<" "<<i.second<<" ";
+
+                    //  }
+                     cout<<endl;
+                     if (mp.find(strs[i])!=mp.end()){
+                        int index=mp[strs[i]];
+                        //   cout<<index<<" "<<endl;
+                          res[index].push_back(fin);
+                     }
+                     else{
+                          res.push_back({fin}); 
+                           mp[strs[i]]=j;
+                           j++;
+                     }
+
+                   
+               }
+
+               return res;
+    }
+};
