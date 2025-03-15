fd709a13f737483ae1301b206d3ef599bc84d53d
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-03-16 00:52:04 +0530

    Time: 170 ms (7.92%) | Memory: 10 MB (41.96%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+
+         int n=s.size();
+         int l=0;
+         int r=n-1;
+         while(l<r){
+                 
+                while (l<r && !isdigit(tolower(s[l])) && (tolower(s[l])<97 || tolower(s[l])>122  ) ){
+                     l++;
+                }
+                while (l<r &&  !isdigit(tolower(s[r])) && (tolower(s[r])<97 || tolower(s[r])>122)  ){
+                     r--;
+                }         
+             cout<<tolower(s[l])<<" "<<tolower(s[r])<<endl;
+                 
+                if( tolower(s[l])!=tolower(s[r])) return false;
+                else{
+                    l++;
+                    r--;
+                }
+                
+         }
+         return true;
+    }
+};
