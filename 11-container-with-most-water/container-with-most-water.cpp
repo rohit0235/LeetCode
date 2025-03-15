7cddebdc491bc8713728dc0b1155333afaada013
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-03-16 00:16:59 +0530

    Time: 0 ms (100.00%) | Memory: 63 MB (13.11%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -3,26 +3,55 @@ public:
     int maxArea(vector<int>& height) {
           
           int n =height.size();
-
+          
+          //    1 8 6 2 5 4 8 3 7
+          int maxarea=0;
           int l=0;
           int r=n-1;
-          int maxi=0;
-          while (l<r){
-                  
-                int h=min(height[l],height[r]);
-                int w=r-l;
-                maxi=max(maxi,h*w);
-
-                if (height[l]<height[r]) {
-                    l++;
-                }
-                else{
-                    r--;
-                }
-
+          while(l<=r){
+               int check=min(height[l],height[r]);
+               maxarea=max(maxarea,check*(r-l));
+               if(height[l]<=height[r]) l++;
+               else r--;  
           }
+          return maxarea;
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //   int l=0;
+        //   int r=n-1;
+        //   int maxi=0;
+        //   while (l<r){
+                  
+        //         int h=min(height[l],height[r]);
+        //         int w=r-l;
+        //         maxi=max(maxi,h*w);
+
+        //         if (height[l]<height[r]) {
+        //             l++;
+        //         }
+        //         else{
+        //             r--;
+        //         }
+
+        //   }
          
-         return maxi;
+        //  return maxi;
 
 
 
