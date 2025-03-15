1a0671191ddc66eab8d6734e831714ee91c6ef8c
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-03-16 00:55:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (79.23%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -5,23 +5,46 @@ public:
          int n=s.size();
          int l=0;
          int r=n-1;
-         while(l<r){
-                 
-                while (l<r && !isdigit(tolower(s[l])) && (tolower(s[l])<97 || tolower(s[l])>122  ) ){
-                     l++;
-                }
-                while (l<r &&  !isdigit(tolower(s[r])) && (tolower(s[r])<97 || tolower(s[r])>122)  ){
-                     r--;
-                }         
-             cout<<tolower(s[l])<<" "<<tolower(s[r])<<endl;
-                 
-                if( tolower(s[l])!=tolower(s[r])) return false;
-                else{
-                    l++;
-                    r--;
-                }
-                
+        
+         while (l<r){ 
+              while (l<r && !isalnum(s[l])){
+                   l++;
+              }
+              
+              while (l<r && !isalnum(s[r])){
+                   r--;
+              }
+
+              if (tolower(s[l])!=tolower(s[r])){
+                 return false;
+              }
+
+              l++;
+              r--;
+              
          }
+
          return true;
+
+
+
+        //  while(l<r){
+                 
+        //         while (l<r && !isdigit(tolower(s[l])) && (tolower(s[l])<97 || tolower(s[l])>122  ) ){
+        //              l++;
+        //         }
+        //         while (l<r &&  !isdigit(tolower(s[r])) && (tolower(s[r])<97 || tolower(s[r])>122)  ){
+        //              r--;
+        //         }         
+        //      cout<<tolower(s[l])<<" "<<tolower(s[r])<<endl;
+                 
+        //         if( tolower(s[l])!=tolower(s[r])) return false;
+        //         else{
+        //             l++;
+        //             r--;
+        //         }
+                
+        //  }
+        //  return true;
     }
 };
