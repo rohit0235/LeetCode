ee166e661c75ccbc44bda32f4a16f80d38c027a3
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-03-18 16:57:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (44.56%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,20 +2,27 @@ class Solution {
 public:
     int n;
     
-    int solve(vector<int>&nums,int index,vector<int>&dp){
+    // int solve(vector<int>&nums,int index,vector<int>&dp){
 
-      if (index>=n) return 0;
-      if (dp[index]!=-1) return dp[index];
-     int  chori=nums[index]+solve(nums,index+2,dp);
-     int  chorinahi=solve(nums,index+1,dp);
-     return dp[index]=max(chori,chorinahi);
+    //   if (index>=n) return 0;
+    //   if (dp[index]!=-1) return dp[index];
+    //  int  chori=nums[index]+solve(nums,index+2,dp);
+    //  int  chorinahi=solve(nums,index+1,dp);
+    //  return dp[index]=max(chori,chorinahi);
 
-    } 
+    // } 
 
     int rob(vector<int>& nums) {
         n=nums.size();
-        vector<int>dp(n+1,-1);
+        vector<int>dp(n+1,0);
+           dp[1] = nums[0];
+        
+        for(int i=2;i<=n;i++){ 
+               
+               dp[i]=max(dp[i-2]+nums[i-1],dp[i-1]);
+        }
 
-        return solve(nums,0,dp);
+        return dp[n];
+        // return solve(nums,0,dp);
     }
 };
