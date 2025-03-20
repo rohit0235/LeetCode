031f78183f580fea25e528ecbcc9ad30335a0749
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-03-21 01:07:09 +0530

    Time: 4 ms (1.52%) | Memory: 11.8 MB (8.94%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int solve(int i,int j,int m,int n,  vector<vector<int>>&dp){
+           
+           if (i==m || j==n ){
+             return 0;
+           }
+           
+           if (i==m-1 && j==n-1) return 1;
+           if (dp[i][j]!=-1) return dp[i][j]; 
+           int pos1=solve(i+1,j,m,n,dp);
+           int pos2=solve(i,j+1,m,n,dp);
+           
+            return dp[i][j]=pos1+pos2;
+    }
+    
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(101,vector<int>(101,-1));
+        return solve(0,0,m,n,dp);
+
+    }
+};
