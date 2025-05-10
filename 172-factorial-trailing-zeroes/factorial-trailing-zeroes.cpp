9c8466da249cfb586bd9e390a305b9165e3fcef4
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-05-10 08:55:47 +0530

    Time: 1 ms (12.84%) | Memory: 8.1 MB (85.75%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -2,29 +2,42 @@ class Solution {
 public:
     int trailingZeroes(int n) {
          
-         int ft=0;
-         int ff=0;
 
-         for(int i=2;i<=n;i++){
+             int temp=5;
+
+             int count=0;
+
+             while (n/temp>=1){
+                  count+=(n/temp);
+                  temp=temp*5;
+             }
+
+             return count;
+
+
+        //  int ft=0;
+        //  int ff=0;
+
+        //  for(int i=2;i<=n;i++){
                  
-                 int temp=i;
-                 while (temp%2==0){
-                     ft++;
-                    temp=temp/2;
+        //          int temp=i;
+        //          while (temp%2==0){
+        //              ft++;
+        //             temp=temp/2;
 
-                 }
-                 temp=i;
-                 while (temp%5==0){
-                     ff++;
-                     temp=temp/5;
+        //          }
+        //          temp=i;
+        //          while (temp%5==0){
+        //              ff++;
+        //              temp=temp/5;
 
-                 }
-
-
-
-         }
+        //          }
 
 
-         return  min(ff,ft);
+
+        //  }
+
+
+        //  return  min(ff,ft);
     }
 };
