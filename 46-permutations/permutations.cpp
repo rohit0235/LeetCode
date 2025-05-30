6bdcb1a22f40f586a11d6ca9a88af93ae1c4288a
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-05-30 18:55:43 +0530

    Time: 1 ms (43.76%) | Memory: 10.8 MB (40.26%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -2,23 +2,40 @@ class Solution {
 public:
     int n;
     vector<vector<int>> res;
-    unordered_set<int> st;
-    void solve(vector<int>& nums, vector<int>& s) {
+    // unordered_set<int> st;
+    // void solve(vector<int>& nums, vector<int>& s) {
 
-        if (s.size() == n) {
-            res.push_back(s);
+    //     if (s.size() == n) {
+    //         res.push_back(s);
+    //         return;
+    //     }
+
+    //     for (int i = 0; i < n; i++) {
+    //         if (st.find(nums[i]) == st.end()) {
+
+    //             st.insert(nums[i]);
+    //             s.push_back(nums[i]);
+    //             solve(nums, s);
+    //             s.pop_back();
+    //             st.erase(nums[i]);
+    //         }
+    //     }
+    // }
+
+    // swapping approach
+
+    void solve(vector<int>& nums, int idx) {
+
+        if (idx == n) {
+            res.push_back(nums);
             return;
         }
 
-        for (int i = 0; i < n; i++) {
-            if (st.find(nums[i]) == st.end()) {
+        for (int i = idx; i < n; i++) {
 
-                st.insert(nums[i]);
-                s.push_back(nums[i]);
-                solve(nums, s);
-                s.pop_back();
-                st.erase(nums[i]);
-            }
+            swap(nums[i], nums[idx]);
+            solve(nums, idx + 1);
+            swap(nums[i], nums[idx]);
         }
     }
 
@@ -26,8 +43,9 @@ public:
 
         n = nums.size();
 
-        vector<int> s;
-        solve(nums,s);
+        // vector<int> s;
+        // solve(nums,s);
+        solve(nums, 0);
 
         return res;
     }
