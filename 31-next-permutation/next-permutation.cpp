7d578b4010958d2a59cc1e00e3e72f122c84870b
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-05-30 19:31:05 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (92.16%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -2,9 +2,38 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
           
-         next_permutation(nums.begin(),nums.end());
+        //  next_permutation(nums.begin(),nums.end());
+             int n=nums.size();
+
+           int gi=-1;
+           for(int i=n-1;i>0;i--){
+                    
+                    if (nums[i]>nums[i-1]){
+                         gi = i-1;
+                         break;
+                    }
+
+           }
          
-    
+            if(gi!=-1) {
+             int  swapindex=gi;
+            
+           for(int i=n-1;i>=gi+1;i--){
+                    
+                    if (nums[i]>nums[gi]){
+                        swapindex=i;
+                         break;
+                    }
+
+           
+
+            }
+                 swap(nums[gi],nums[swapindex]);
+            }
+
+            reverse(nums.begin()+gi+1,nums.end());
+
+            
 
     }
 };
