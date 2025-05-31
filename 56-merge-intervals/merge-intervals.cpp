de7070c971872eaf6c3c2ef9cc066c5c72ddc0b1
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-05-31 21:46:34 +0530

    Time: 7 ms (43.50%) | Memory: 24 MB (34.95%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,24 +1,55 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-         sort(intervals.begin(),intervals.end());
+          sort(intervals.begin(),intervals.end());
           vector<vector<int>>result;
           int n=intervals.size();
-          int i=0;
-          while (i<n){
-                  int start=intervals[i][0];
-                  int j=i+1;
-                  int end=intervals[i][1];
-                  while (j<n && end>=intervals[j][0]){
-                        end=max(end,intervals[j][1]);
-                        j++;
-                  }
-                  result.push_back({start,end});
-                  i=j;
-                  
-          }
+           
 
+          for(int i=0;i<n;i++){
+              
+              if (result.empty() || result.back()[1]<intervals[i][0]){
+                   result.push_back(intervals[i]);
+
+              }
+
+              else{
+                 result.back()[1]=max(result.back()[1],intervals[i][1]);
+              }
+              
+
+          }
           return result;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //   int i=0;
+        //   while (i<n){
+        //           int start=intervals[i][0];
+        //           int j=i+1;
+        //           int end=intervals[i][1];
+        //           while (j<n && end>=intervals[j][0]){
+        //                 end=max(end,intervals[j][1]);
+        //                 j++;
+        //           }
+        //           result.push_back({start,end});
+        //           i=j;
+                  
+        //   }
+
+        //   return result;
 
     }
 };
