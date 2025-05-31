05b918ff9f983b13101fc6737e65ba23d7aa5b36
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-06-01 00:25:53 +0530

    Time: 339 ms (9.85%) | Memory: 229 MB (5.77%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        priority_queue<vector<int>>pq;
+        vector<int>ans;
+        for(int i=0;i<k;i++){
+             pq.push({nums[i],i});
+        }
+        ans.push_back(pq.top()[0]);
+         int n=nums.size();
+        for(int i=k;i<n;i++){
+            
+            pq.push({nums[i],i});
+            while (!pq.empty() && pq.top()[1]<=i-k){
+                pq.pop();
+            }
+            
+            ans.push_back(pq.top()[0]);
+
+
+        }
+        
+        return ans;
+ 
+
+
+    }
+};
