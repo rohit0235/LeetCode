9929189a9cd4a7691596582420442fa6d273a040
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-06-02 18:39:49 +0530

    Time: 25 ms (83.73%) | Memory: 70.8 MB (92.42%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,12 +1,20 @@
+
+
+struct comp{
+      
+        bool operator()(int a ,int b){
+             return a<b;       
+          }
+};
 class Solution {
 public:
-   
-    static bool myc(int a ,int b){
-     return a<b;
-    }
+
+    // static bool myc(int a ,int b){
+    //  return a<b;
+    // }
     
     vector<int> sortArray(vector<int>& vec) {
-             sort(begin(vec),end(vec),myc);
+             sort(begin(vec),end(vec),comp());
              return vec;
     }
 };
