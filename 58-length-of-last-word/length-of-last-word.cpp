2b420ed3a679532a4350b1353b215b759943593e
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-06-02 18:56:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (5.73%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-               int n= s.size()-1;
-             int i=n;
-             while (i>=0 && s[i]==' '){
-                 i--;
-             }
-             int len=0;
-             while (i>=0 && s[i]!=' '){
-                 len++;
-                 i--;
-             }
+             int n= s.size()-1;
+            //  int i=n;
+            //  while (i>=0 && s[i]==' '){
+            //      i--;
+            //  }
+            //  int len=0;
+            //  while (i>=0 && s[i]!=' '){
+            //      len++;
+            //      i--;
+            //  }
 
-             return len;
+            //  return len;
+            
+            stringstream ss(s);
 
+              string word;
+            int len=0;
+            while (ss>>word){
+                 len=word.size();
+            }
 
+            return len;
+                
     }
 };
