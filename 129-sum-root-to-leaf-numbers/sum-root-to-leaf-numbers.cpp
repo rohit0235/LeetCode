1b554bf512835f8a6f82b9696ac23181f319579e
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-06-04 19:00:17 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (43.52%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -13,41 +13,59 @@ class Solution {
 public:
       vector<vector<int>>result;
 
-      void  solve(TreeNode* root,vector<int>&path){
-            if (root==NULL) return ;
 
+      int  solve(TreeNode* root,int current){
+            if (root==NULL) return 0 ;
+     current=current*10+(root->val);
             if (root->left==NULL && root->right==NULL) {
-                   path.push_back(root->val);
-                result.push_back(path); path.pop_back();
-                return ;
+                //    path.push_back(root->val);
+                // result.push_back(path); path.pop_back();
+                return current;
             }
-            path.push_back(root->val);
-            solve(root->left,path);
-            solve(root->right,path);
-             path.pop_back();
+            // path.push_back(root->val);
+        
             
+            int l=solve(root->left,current);
+            int r =solve(root->right,current);
+            //  path.pop_back();
+            return l+r;
              
       }
   
+    //   void  solve(TreeNode* root,vector<int>&path){
+    //         if (root==NULL) return ;
 
+    //         if (root->left==NULL && root->right==NULL) {
+    //                path.push_back(root->val);
+    //             result.push_back(path); path.pop_back();
+    //             return ;
+    //         }
+    //         path.push_back(root->val);
+    //         solve(root->left,path);
+    //         solve(root->right,path);
+    //          path.pop_back();
+            
+             
+    //   }
+  
   
     int sumNumbers(TreeNode* root) {
            
-           vector<int>path;
+        //    vector<int>path;
 
-           solve(root,path);
-             int sum=0;
-           for(auto i:result){
-                 int num=0;
-                for (auto j:i){
-                    num=num*10+j;
-                }
-                sum+=num;
+         return   solve(root,0);
+        //      int sum=0;
+        //    for(auto i:result){
+        //          int num=0;
+        //         for (auto j:i){
+        //             num=num*10+j;
+        //         }
+        //         sum+=num;
 
-                // cout<<endl;
-           }
+        //         // cout<<endl;
+        //    }
         
-           return sum;
+           
 
     }
 };
