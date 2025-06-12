46899619440b954952484b1a8841fc28f8185d1d
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-06-13 01:02:48 +0530

    Time: 8 ms (39.30%) | Memory: 19.4 MB (65.16%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool solve(vector<int>& visited, vector<int>& dfsvisited,
+               vector<vector<int>> &adj, int u) {
+
+        visited[u] = true;
+        dfsvisited[u] = true;
+
+        for (int v : adj[u]) {
+
+            if (!visited[v]) {
+                if (solve(visited, dfsvisited, adj, v)) {
+                    return true;
+                }
+            }
+
+            if (dfsvisited[v]) {
+                return true;
+            }
+        }
+
+        dfsvisited[u] = false;
+        return false;
+    }
+    bool canFinish(int V, vector<vector<int>>& prerequisites) {
+
+        vector<vector<int>> adj(V);
+
+        for (auto& vec : prerequisites) {
+
+            int u = vec[0];
+            int v = vec[1];
+
+            adj[v].push_back(u);
+        }
+
+        vector<int> visited(V, 0);
+        vector<int> dfsvisited(V, 0);
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                if (solve(visited, dfsvisited, adj, i)){
+                       return false;
+                }
+                 
+       
+                
+            }
+        }
+
+        return true;
+    }
+};
