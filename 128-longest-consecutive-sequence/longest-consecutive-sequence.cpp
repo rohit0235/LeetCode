8b47e907bedd9bc5003bba5d9f261771d42065d9
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-06-13 19:49:53 +0530

    Time: 84 ms (53.25%) | Memory: 88.9 MB (29.40%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,44 +1,38 @@
 class Solution {
 public:
-
     int longestConsecutive(vector<int>& nums) {
-        
-               int len=1;
-               int maxi=0;
-               if (nums.size()==0) return false;
-               if (nums.size()==1) return true;
-               set<int>s;
-                 for (int i=0;i<nums.size();i++){
-                          s.insert(nums[i]);
-                 }
-                 
-                 for (auto i:s){
-                   
-                           if (s.find(i+1)!=s.end()){
-                                  len+=1;
-                           }
-                           else{
-                                len=1;
-                           }
-                           maxi=max(maxi,len);
 
+        if (nums.size() == 0)
+            return false;
+        if (nums.size() == 1)
+            return true;
+        unordered_set<int> s;
+        for (int i = 0; i < nums.size(); i++) {
+            s.insert(nums[i]);
+        }
+        int maxi = INT_MIN;
 
-                 }
+        for (auto i : s) {
 
-                 return maxi;
+            if (s.find(i - 1) == s.end()) {
+                int len = 1;
+                int x = i;
+                while (s.find(x) != s.end()) {
+                    len=len+1;
+                    x=x+1;
+                }
+                maxi = max(maxi, len);
+            }
+        }
 
+        return maxi-1;
 
+        //    map<int,int>mp;
+        //     int n=nums.size();
+        //    for (int i=0;i<n;i++) mp[i]++;
 
-            //    map<int,int>mp;
-            //     int n=nums.size();
-            //    for (int i=0;i<n;i++) mp[i]++;
-
-            //    for(auto i:mp){
-            //             if ()
-            //    }
-
-
-
-
+        //    for(auto i:mp){
+        //             if ()
+        //    }
     }
 };
