d93750b0fd7c9307ee524a42208d371f96c9eab4
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-06-25 15:02:05 +0530

    Time: 179 ms (95.62%) | Memory: 10.4 MB (87.02%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,43 +1,93 @@
 class Solution {
 public:
-         vector<vector<int>>dire={{-1,0},{0,-1},{1,0},{0,1}};
-  bool find(vector<vector<char>>& board, string &word,int i,int j,int index,int n,int m){
-                 
-                 if (index==word.size()) return true;
-                 
-            if (i<0 || i>=n || j<0 || j>=m || board[i][j]=='$') return false;
-                 if (board[i][j]!=word[index]) return false;
-                  char temp=board[i][j];
-                  board[i][j]='$';
-                 for (auto &dir:dire){
-                       
-                       int newx=i+dir[0];
-                       int newy=j+dir[1];
-                        if (find(board,word,newx,newy,index+1,n,m)){
-                            return true;
-                        }
-                     
-                 }
-                    board[i][j]=temp;
+
+int n,m,w;
+
+bool dfs(vector<vector<char>>& board, int i ,  int j , string& word , int idx ){
+     
+
+            if (idx>=word.length()) return true;
+
+            if (i<0 || i>=n || j<0 ||  j>=m || board[i][j]!=word[idx]){
                  return false;
-                 
-    }
+            }
+
+            char current = board[i][j];
+            board[i][j]='#';
+
+            bool f = dfs(board, i+1,j,word,idx+1)
+                   || dfs(board, i,j+1,word,idx+1)
+                  || dfs(board, i,j-1,word,idx+1)
+                || dfs(board, i-1,j,word,idx+1);
+            
+            board[i][j]=current;
+
+
+            return f;
+
+}
     bool exist(vector<vector<char>>& board, string word) {
-           
-           int n =board.size();
-           int m=board[0].size();
+             
+                  n =board.size();
+                 m =board[0].size();
 
-           for (int i=0;i<n;i++){
-                
-                for (int j=0;j<m;j++){
+                  w =word.size();
+                 
+                //  queue<pair<int,int>>q;
+                 for(int i=0;i<n;i++){
                       
-                      if (board[i][j]==word[0]  && find(board,word,i,j,0,n,m)){
-                           return true;
-                      }
-                }
-           }
+                        for(int j=0;j<m;j++){
+                             
+                              if (board[i][j]==word[0]){
+                                //    q.push({i,j});
 
-           return false;
+                                      if (dfs(board, i , j ,word, 0)) return true;
+
+                              }
+                        }
+                 }
+
+                 return false;
+                
+                //  int j=1;
+                //  while (!q.empty()){
+                //               if (j>=w) return true;
+                //               int  k = q.size();
+                //             //   int h =j;
+                //               bool flag = false;
+                //               for(int s=0;s<k;s++){
+                                   
+                //                       pair<int,int>top = q.front();
+                //                       q.pop();
+                //                       int i =top.first;
+                //                       int newj =top.second;
+                                      
+                //                       if (board[i][newj]==word[j]){
+                                        
+                //                          if (i+1<n) q.push({i+1,newj});
+                //                          if (newj+1<m) q.push({i,newj+1});
+                                      
+                //                          if (i-1>=0)  q.push({i-1,newj});
+                //                          if (newj-1>=0)  q.push({i,newj-1});
+                //                          flag=true;
+                                           
+                //                       }
+                                    
+                                  
+
+                                   
+                //               }
+                //               if (flag)j++;
+                //             //   else return false;
+                //             //   if (h==j) return false;
+                              
+
+
+                //  }
+
+                //  return j>=w;
+                 
+
 
 
     }
