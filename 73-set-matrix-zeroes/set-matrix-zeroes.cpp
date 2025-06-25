c419d8fe6093eed1892206bec1e680550fb38adb
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-06-25 12:26:57 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (24.48%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-               
-              
-                int n=matrix.size();
-                int m=matrix[0].size();
-                 vector<bool>c(m,0);
-                 vector<bool>r(n,0);
-                for(int i=0;i<n;i++){
-                      
-                      for(int j=0;j<m;j++){
-                             if (matrix[i][j]==0){
-                                   
-                                   c[j]=1;
-                                   r[i]=1;
+        
+             unordered_set<int>row;
+             unordered_set<int>col;
+             int m =matrix.size();
+             int n =matrix[0].size();
+             for(int i=0;i<m;i++){
+                 
+                  for(int j=0;j<n;j++){
+                        
+                          if (matrix[i][j]==0){
+                                row.insert(i);
+                                col.insert(j);
+                          }
+                        
+                  }
+             }
 
-
-                             }
-                             
+             for(int i=0;i<m;i++){
+                 
+                  for(int j=0;j<n;j++){
+                        
+                                                                                          if ((row.count(i) || col.count(j))){
+                                matrix[i][j]=0;
                       }
-                }
-                for(int i=0;i<n;i++){
-                      
-                      for(int j=0;j<m;j++){
-                             if (c[j]==1 || r[i]==1){
-                                   matrix[i][j]=0;
-                             }
-                             
-                      }
-                }
 
-
-
+                        
+                  }
+             }
 
     }
 };
