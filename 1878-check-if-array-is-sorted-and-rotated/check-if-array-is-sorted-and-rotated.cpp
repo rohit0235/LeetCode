6a6ae657aecb58a69d9526a1cf8805a84a99ba36
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-06-28 23:24:50 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (13.91%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
-     bool issorted(vector<int>& nums){
-               for (int i=0;i<nums.size()-1;i++){
-                    if (nums[i]>nums[i+1]){
-                          return false;
-                    }
-               }
-               return true;
-     }
     bool check(vector<int>& nums) {
-                        int n =nums.size();
-                  int count=0;
-                  if (issorted(nums)) return true;
-                  for (int i=0;i<n-1;i++){
-                        if (nums[i]>nums[i+1]){
-                            count=i;
-                            break;
-                        }
-                  }
-                     reverse(nums.begin(),nums.begin()+count+1);
-                reverse(nums.begin()+count+1,nums.end());
-           
-                reverse(nums.begin(),nums.end());   
-                if (issorted(nums)) return true;
-                else return false;
-                  
-      
- 
+         
+           int count =0;
+         
+            for(int i=0;i<nums.size()-1;i++){
+                   if (nums[i]>nums[i+1]){
+                      count++;
 
-    }
+                   }
+
+                 
+            }
+           if (count==0) return true;
+           if (count ==1 && nums[0]>=nums[nums.size()-1]) return true;
+
+           return false;
+          
+    } 
 };
