1e6703c8ace5742e988035ae8ef4a1fe8a04a159
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-06-30 02:30:19 +0530

    Time: 11 ms (6.94%) | Memory: 13 MB (14.88%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -2,26 +2,47 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
               
-            int n=nums2.size();
-            vector<int>ans(n,-1);
-            stack<int>st;
-            for(int i=n-1;i>=0;i--){
-                  while (!st.empty() && st.top()<=nums2[i]){
-                       st.pop();
-                  }
+               int n =nums2.size();
+             vector<int>nextgreater(n,-1);
+          stack<int>pq;
 
-                 if (!st.empty()) ans[i]=st.top();
-                  st.push(nums2[i]);
+            //  pq.push(-1);
+            //  nextgreater[n-1]=-1;
 
-            }
-            unordered_map<int,int>mp;
-            for(int i=0;i<nums2.size();i++) mp[nums2[i]]=i;
-            vector<int>result;
-            for(int i:nums1) {
-                   result.push_back(ans[mp[i]]);
-            }
+             for(int i=n-1;i>=0;i--){
+                      
+                      while  (!pq.empty() && nums2[i]>=pq.top()){
+                      
+                             pq.pop();
+                          
+                      }
+                     
+                      if (pq.empty()){
+                          nextgreater[i]=-1;
+                      }
+                      else{
+                          nextgreater[i]=pq.top();
+                      }
+                       pq.push(nums2[i]);
 
-            return result;
-        
+             }
+     
+             for(int i:nextgreater) cout<<i<<endl;
+             unordered_map<int,int>mp;
+
+              for(int i=0;i<n;i++){
+                mp[nums2[i]]=i;
+             }
+
+             vector<int>ans;
+             for(int i:nums1){
+                if (i<nextgreater[mp[i]])
+                 ans.push_back(nextgreater[mp[i]]);
+                 else            ans.push_back(-1);
+             }
+               return ans;
+
+
+          
     }
 };
