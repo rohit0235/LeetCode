ca9f751b258decec13bfa39cde532f1d49c7cf5a
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-07-01 20:03:18 +0530

    Time: 4 ms (86.10%) | Memory: 31 MB (58.23%) - LeetSync

---
+++ 221-maximal-square/maximal-square.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+           int m =matrix.size();
+           int n =matrix[0].size();
+           vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        //  if (m==1 && matrix[0][0]=='0') return 0;
+        //  if (m==1 && matrix[0][0]=='1') return 1;
+        //    dp[0][0]=0;
+           int maxSize=0;
+           for(int i=0;i<m;i++){
+              
+                      for(int j=0;j<n;j++){
+                          
+                           if (matrix[i][j]=='1'){
+                              
+ dp[i + 1][j + 1] = std::min(std::min(dp[i][j + 1], dp[i + 1][j]), dp[i][j]) + 1;
+           
+                    maxSize = std::max(maxSize, dp[i + 1][j + 1]);
+                              
+                           }
+                                    
+                          
+                      } 
+
+              
+           }
+
+           return  maxSize*maxSize;
+
+
+    }
+};
