0db9efeedc48fd618a28ea00412b6b6a7cd581f0
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-07-04 19:47:44 +0530

    Time: 7 ms (46.24%) | Memory: 9.7 MB (36.86%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -2,26 +2,62 @@ class Solution {
 public:
 
       
-        int dp[1001][1001];
-      int solve(int copy , int clip , int n){
+    //     int dp[1001][1001];
+    //   int solve(int copy , int clip , int n){
            
-            if (copy==n) return 0;
+    //         if (copy==n) return 0;
 
-            if (copy>n) return 1000000;
+    //         if (copy>n) return 1000000;
 
-            if (dp[copy][clip]!=-1) return dp[copy][clip];
+    //         if (dp[copy][clip]!=-1) return dp[copy][clip];
 
-            int copyandpaste =1+1+ solve(copy+copy,copy , n);
-            int paste= 1+solve(copy+clip ,clip , n);
+    //         int copyandpaste =1+1+ solve(copy+copy,copy , n);
+    //         int paste= 1+solve(copy+clip ,clip , n);
 
-            return dp[copy][clip]=min(copyandpaste,paste )   ;         
+    //         return dp[copy][clip]=min(copyandpaste,paste )   ;         
             
-      }
+    //   }
 
     int minSteps(int n) {
         if (n==1) return 0;
-        memset(dp,-1,sizeof(dp));
 
-        return 1+solve(1,1,n);
+
+         vector<int>t(n+1,0);
+
+         t[0]=0;
+         t[1]=0;
+         t[2]=2;
+
+         for(int i=3;i<=n;i++){
+             
+              
+                int factor = i/2;
+                
+                while (factor>=1){
+                     
+                      
+                           if (i%factor==0){
+                              
+                               
+                               int paste  = (i/factor)-1;
+                               t[i]= t[factor]+1+paste; 
+                                break;
+                           }
+                           else{
+                             factor--;
+                           }
+                        
+                         
+                }
+                
+         }
+
+         return t[n];
+
+
+
+        // memset(dp,-1,sizeof(dp));
+
+        // return 1+solve(1,1,n);
     }
 };
