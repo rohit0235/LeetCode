dbdea09361ee246f0cbbcfcbe4d24054504cda73
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-07-04 19:29:29 +0530

    Time: 11 ms (40.95%) | Memory: 12.3 MB (25.62%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
 
+      
+        int dp[1001][1001];
       int solve(int copy , int clip , int n){
            
             if (copy==n) return 0;
 
             if (copy>n) return 1000000;
 
+            if (dp[copy][clip]!=-1) return dp[copy][clip];
+
             int copyandpaste =1+1+ solve(copy+copy,copy , n);
             int paste= 1+solve(copy+clip ,clip , n);
 
-            return min(copyandpaste,paste )   ;         
+            return dp[copy][clip]=min(copyandpaste,paste )   ;         
             
       }
 
     int minSteps(int n) {
         if (n==1) return 0;
-
+        memset(dp,-1,sizeof(dp));
 
         return 1+solve(1,1,n);
     }
