ecdc5982789d63a7e497b23c9481ae3672fd25ac
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-07-06 18:32:21 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (52.11%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,27 +1,48 @@
 class Solution {
 public:
-    int n;
-    int dp[201][201];
-     int solve (vector<vector<int>>& triangle ,  int i , int j ){
+    // int n;
+    // int dp[201][201];
+    //  int solve (vector<vector<int>>& triangle ,  int i , int j ){
           
-              if (i>=n ) return 0;
-            // sum+=triangle[i][j];
-            //  int first =0;
-            //   int second =0;
-             if (dp[i][j]!=-1) return dp[i][j];
-           int first  =triangle[i][j]+ solve(triangle,i+1,j); 
-          int second  = triangle[i][j]+ solve(triangle,i+1,j+1);
+    //           if (i>=n ) return 0;
+    //         // sum+=triangle[i][j];
+    //         //  int first =0;
+    //         //   int second =0;
+    //          if (dp[i][j]!=-1) return dp[i][j];
+    //        int first  =triangle[i][j]+ solve(triangle,i+1,j); 
+    //       int second  = triangle[i][j]+ solve(triangle,i+1,j+1);
 
-              return dp[i][j]=min(first,second);
+    //           return dp[i][j]=min(first,second);
 
  
            
              
-     }
+    //  }
     int minimumTotal(vector<vector<int>>& triangle) {
-           memset(dp,-1, sizeof(dp));
-             n= triangle.size();
-            return solve(triangle, 0,0);
+      
+            int n = triangle.size();
+            int m = triangle[n-1].size();
+      
+            vector<vector<int>>dp(n,vector<int>(m,1e9));
+            dp[0][0] = triangle[0][0];
+            for( int i = 1; i < n;i++){
+                for( int j = 0; j < triangle[i].size();j++){
+                    if(j <= triangle[i-1].size()){
+                        dp[i][j] = min(dp[i][j],triangle[i][j]+dp[i-1][j]);
+                    }
+                    if( j-1 >= 0){
+                        dp[i][j] = min(dp[i][j],triangle[i][j]+dp[i-1][j-1]);
+                    }
+                }
+            }
+
+            int res = INT_MAX;
+
+            for( int i = 0; i < triangle[n-1].size();i++){
+                res = min(res,dp[n-1][i]);
+            }
+
+            return res;
         
           
 
