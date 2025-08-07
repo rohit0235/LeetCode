30fa09b8a747f4cfd4e373d0fcd84b9582f8906e
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-08-07 23:55:31 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (86.37%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,62 +1,19 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-         if (nums.size()==0) return {-1,-1};
+              
 
-         int n=nums.size();
+                         int n = nums.size();
 
-         int firstidx=-1;
-         int i=0;
-         int j=n-1;
-         while (i<=j){
-             int mid=i+(j-i)/2;
-             if (nums[mid]==target){
-                firstidx=mid;
-                j=mid-1;
-             }
+                int lowerbound = lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+                 int uprbound = upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+                  
+                if (lowerbound>=n) return {-1,-1};
+                if (nums[lowerbound]!=target) return {-1,-1};
 
-             else if(nums[mid]<target){
-                 i=mid+1;
-             }
 
-             else{
+                return {lowerbound , uprbound-1};
+                   
 
-                j=mid-1;
-             }
-         }
-         
-         i=0;
-          j=n-1;
-         
-         int secondidx=-1;
-         while (i<=j){
-             int mid=i+(j-i)/2;
-             if (nums[mid]==target){
-                secondidx=mid;
-                i=mid+1;
-             }
-
-             else if(nums[mid]<target){
-                 i=mid+1;
-             }
-
-             else{
-
-                j=mid-1;
-             }
-         }
-         
-         return {firstidx,secondidx};
-
-        //   int minidx= lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-        //   int maxidx= upper_bound(nums.begin(),nums.end(),target)-nums.begin();
-        //   if (nums[minidx]!=target){
-        //      minidx=-1;
-        //   } 
-        //    if(maxidx>0 && nums[maxidx-1]!=target){
-        //           maxidx=-1;
-        //     }
-        //   if (maxidx!=-1)    return {minidx,maxidx-1};
-        //      return {minidx,maxidx};
     }
 };
