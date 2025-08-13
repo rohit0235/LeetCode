773adc939d5defb0f8b1bce25e6a22b3708724c8
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-08-14 01:23:22 +0530

    Time: 139 ms (30.34%) | Memory: 121.9 MB (20.51%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
 
+unordered_map<string , int>mp;
+
 bool ispos(string a, string b){
      
           vector<int>t1(26,0);
@@ -8,7 +10,7 @@ bool ispos(string a, string b){
          
           for(char i:a) t1[i-'a']++;
           for(char i:b) t2[i-'a']++;
-
+           
           for(int i=0;i<26;i++){
                if (t1[i]>0 &&  t2[i]>0 || t1[i]>1 || t2[i]>1) return true;
           }
@@ -20,7 +22,7 @@ bool ispos(string a, string b){
     int solve(vector<string>&arr , int n, string temp , int i ){
             
              if (i>=n)  return temp.length();
-
+            if (mp.find(temp)!=mp.end())  return mp[temp];
              int inc=  0;
              int exc =0;
 
@@ -32,7 +34,7 @@ bool ispos(string a, string b){
                    inc = solve(arr, n , temp+arr[i], i+1);
              }
 
-             return max(inc , exc);
+             return mp[temp]=max(inc , exc);
            
             
     }
