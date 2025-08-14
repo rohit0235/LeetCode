393dbc8071d624e54fcc105dd284d0e30d9eae57
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-08-14 09:33:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (22.96%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -2,31 +2,51 @@ class Solution {
 public:
     string largestGoodInteger(string num) {
             string ans ="";
-          unordered_map<char,int>mp;
+        //   unordered_map<char,int>mp;
 
-          for(int i=0;i<3;i++)mp[num[i]]++;
-          if (mp.size()==1){
-                 string k = num.substr(0,3);
-                 ans =k;
-          }
+        //   for(int i=0;i<3;i++)mp[num[i]]++;
+        //   if (mp.size()==1){
+        //          string k = num.substr(0,3);
+        //          ans =k;
+        //   }
           
          int n = num.size();
-          for(int i=3;i<n;i++){
+        //   for(int i=3;i<n;i++){
                   
-                  mp[num[i-3]]--;
-                  if (mp[num[i-3]]==0) mp.erase(num[i-3]);
-                  mp[num[i]]++;
-                        //  cout<<mp.size();
-                  if (mp.size()==1){
-                        string k = num.substr(i-2,3);
-                    //    cout<<ans<<" ";
-                        if (ans<k){
-                             ans =k;
-                        }  
-                  }
+        //           mp[num[i-3]]--;
+        //           if (mp[num[i-3]]==0) mp.erase(num[i-3]);
+        //           mp[num[i]]++;
+        //                 //  cout<<mp.size();
+        //           if (mp.size()==1){
+        //                 string k = num.substr(i-2,3);
+        //             //    cout<<ans<<" ";
+        //                 if (ans<k){
+        //                      ans =k;
+        //                 }  
+        //           }
                 
 
-          }
+        //   }
+
+             int i=0;
+             int j=1;
+             int k=2;
+
+             while (k<n){
+                 
+                     if (num[i]==num[j] && num[j]==num[k]) {
+                             string k = num.substr(i,3);
+                             if (ans<k)ans=k;
+                     } 
+
+                     i++;
+                     j++;
+                     k++;
+                 
+                    
+             }
+
+
 
           return ans; 
       
