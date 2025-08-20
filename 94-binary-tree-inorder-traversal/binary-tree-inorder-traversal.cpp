523ac03310c50ac99f972406121143758b7a3d5e
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-08-20 22:25:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (30.76%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -10,20 +10,26 @@
  * };
  */
 class Solution {
-public: 
+public:
+   
+    void solve(vector<int>&ans, TreeNode *root){
+         
+           if (root==NULL) return ;
+           solve(ans,root->left);
+           ans.push_back(root->val);
+        
+           solve(ans,root->right);
 
-         void inorder(TreeNode *root,vector<int>&ans){ 
-                
-                if (root==NULL) return ;
-                      inorder(root->left , ans);
-                   ans.push_back(root->val);
-                  inorder(root->right , ans);
-         }
- 
+    }
+    
     vector<int> inorderTraversal(TreeNode* root) {
-              
-              vector<int>ans;
-              inorder(root,ans);
-              return ans;
+         if (root==NULL) return {};
+
+
+         vector<int>ans;
+         solve(ans, root);
+          return ans;
+
+
     }
 };
