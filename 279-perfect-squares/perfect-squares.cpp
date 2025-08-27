156cbeefe41a1de849b83de37c25957a1ee0c1b9
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-08-28 01:00:20 +0530

    Time: 704 ms (5.01%) | Memory: 204.1 MB (5.01%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
-     
-     int solve(vector<int>&temp,int n,int i,int sum,  vector<vector<int>>&dp){
-       
-                  if (sum==n) return 0;
-                  if (sum>n || i>=temp.size()) return 1e9;
-                   if (dp[i][sum]!=-1) return dp[i][sum];
 
-                   int temp1=1e9;   
-                //    int temp2=0;   
-                  if (sum+temp[i]<=n){
-                    temp1= 1+solve(temp,n,i,sum+temp[i],dp);
-                  }    
-                
-                 int temp2= solve(temp,n,i+1,sum,dp);
-                     
+    int solve(int n , int i, vector<int>&temp, vector<vector<int>>&dp){
+             if (n==0) return 0;
+             if (n<0) return 1e9;
+             if (i>=temp.size()) return 1e9;
+             if (dp[n][i]!=-1) return dp[n][i];
+            //   int first=0 ;
+            
+            int first =1+ solve(n-temp[i],i,temp , dp);
+             int second = solve(n,i+1,temp, dp);
 
+                //  cout<<first<<" "<<second;
+             return dp[n][i] = min(first , second);
 
-
-                  return  dp[i][sum]=min(temp1,temp2);
-     }
- 
-    
+           
+    }
     int numSquares(int n) {
-
-               vector<int>temp;
-               for(int i=1;i*i<=n;i++) {
-                    temp.push_back(i*i);
-               }    
-               int p =temp.size();
-               vector<vector<int>>dp(p,vector<int>(n,-1));
-               return solve(temp,n,0,0,dp);
-    
+           
+            vector<int>temp;
+            for(int i=1;i*i<=n;i++){
+                   temp.push_back(i*i);
+            }
+            
+            vector<vector<int>>dp(n+1, vector<int>(temp.size(),-1));
+            return solve(n,0,temp ,dp );
+           
     }
 };
