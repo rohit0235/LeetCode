18533e36f0bfe8e298e9f309c336f16ffa23b8a5
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-08-30 11:09:58 +0530

    Time: 1 ms (69.98%) | Memory: 24.2 MB (47.81%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,66 +1,53 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-            
-            int n =board.size();
-            int m =board[0].size();
-            for(int i=0;i<n;i++){
-                  unordered_set<char>st1;
-                  unordered_set<char>st2;
-                  for(int j=0;j<m;j++){
-                                 if (board[i][j]=='.') continue;
 
-                        if (st1.find(board[i][j])!=st1.end()){
-                              return false;
-                        }
-                        // if (st2.find(board[j][i])!=st2.end()){
-                        //       return false;
-                        // }
+        int n = board.size();
+        int m = board[0].size();
+
+        for (int i = 0; i < n; i++) {
+            unordered_set<char> st1;
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == '.')
+                    continue;
+
+                if (st1.find(board[i][j]) != st1.end())
+                    return false;
 
                         st1.insert(board[i][j]);
-                        // st2.insert(board[j][i]);
-                  }
+            }
+        }
 
-            } 
+        for (int i = 0; i < n; i++) {
+            unordered_set<char> st2;
+            for (int j = 0; j < m; j++) {
+                if (board[j][i] == '.')
+                    continue;
 
-            for(int i=0;i<n;i++){
-                //   unordered_set<char>st1;
-                  unordered_set<char>st2;
-                  for(int j=0;j<m;j++){
-                                 if (board[j][i]=='.') continue;
+                if (st2.find(board[j][i]) != st2.end())
+                    return false ;
+                 st2.insert(board[j][i]);
+            }
+        }
+   
+        for (int i = 0; i < 9; i += 3) {
 
-                        // if (st1.find(board[i][j])!=st1.end()){
-                        //       return false;
-                        // }
-                        if (st2.find(board[j][i])!=st2.end()){
-                              return false;
+            for (int j = 0; j < 9; j += 3) {
+                   unordered_set<char> st; 
+                for (int row = i; row < i + 3; row++) {
+                    for (int col = j; col < j + 3; col++) {
+                        if (board[row][col] == '.') {
+                            continue;
                         }
-
-                        // st1.insert(board[i][j]);
-                        st2.insert(board[j][i]);
-                  }
-
-            } 
-for (int i = 0; i < 9; i += 3) { // Iterate through starting rows of 3x3 blocks
-                for (int j = 0; j < 9; j += 3) { // Iterate through starting columns of 3x3 blocks
-                    unordered_set<char> st; // Set for current 3x3 block
-                    for (int row = i; row < i + 3; row++) {
-                        for (int col = j; col < j + 3; col++) {
-                            if (board[row][col] == '.') {
-                                continue;
-                            }
-                            if (st.find(board[row][col]) != st.end()) {
-                                return false; // Duplicate found in this 3x3 block
-                            }
-                            st.insert(board[row][col]);
+                        if (st.find(board[row][col]) != st.end()) {
+                            return false; // Duplicate found in this 3x3 block
                         }
+                        st.insert(board[row][col]);
                     }
                 }
             }
+        }
 
-
-            return true;
-
-             
+        return true;
     }
 };
