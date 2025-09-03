4937cce5936d7425a7c8d88aee644e7c61d6047d
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-09-03 23:45:14 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (92.58%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -0,0 +1,31 @@
+
+
+class Solution {
+public:
+    int minFallingPathSum(std::vector<std::vector<int>>& matrix) {
+        int n = matrix.size();
+        if (n == 0) return 0;
+        int m = matrix[0].size();
+        if (m == 0) return 0;
+        
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                int minAbove = matrix[i-1][j];
+                if (j > 0) {
+                    minAbove = std::min(minAbove, matrix[i-1][j-1]);
+                }
+                if (j < m - 1) {
+                    minAbove = std::min(minAbove, matrix[i-1][j+1]);
+                }
+                matrix[i][j] += minAbove;
+            }
+        }
+        
+        int minSum = INT_MAX;
+        for (int j = 0; j < m; ++j) {
+            minSum = std::min(minSum, matrix[n-1][j]);
+        }
+        
+        return minSum;
+    }
+};
