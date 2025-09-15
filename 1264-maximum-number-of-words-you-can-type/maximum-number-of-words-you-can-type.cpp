b5f9c6e4e3ebd583d400384ac1def9949d287fcb
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-09-15 10:37:38 +0530

    Time: 1 ms (33.89%) | Memory: 8.8 MB (79.40%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
@@ -2,29 +2,41 @@ class Solution {
 public:
     int canBeTypedWords(string text, string brokenLetters) {
         
-            stringstream ss(text);
+            // stringstream ss(text);
 
             vector<int>check(26,0);
             for(char i:brokenLetters)check[i-'a']++;
-
-
-            string token;
+            bool find = true;
             int count =0;
-            while (ss>>token){
+            for(char i:text){ 
+                  
+                    if (i==' '){
+                         if (find) count++;
+                         find = true;
+                    }
+                    else if (check[i-'a']) find = false;
+                  
+            }
+             if (find) count++;
+            return count;
+
+            // string token;
+            // int count =0;
+            // while (ss>>token){
                     
-                     string c =token;
-                     bool find = false;
-                     for(char i:c){ 
-                          if (check[i-'a']) {
-                             find =true;
-                             break;
-                          }
-                     }
-                     if (!find)count++;
+            //          string c =token;
+            //          bool find = false;
+            //          for(char i:c){ 
+            //               if (check[i-'a']) {
+            //                  find =true;
+            //                  break;
+            //               }
+            //          }
+            //          if (!find)count++;
 
                    
-            }
+            // }
 
-            return count;
+            // return count;
     }
 };
