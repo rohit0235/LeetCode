b713fc7ec7e91bcdf87aebe7ad5d40c0ad35190f
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-10-05 00:52:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (98.64%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.cpp
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -11,15 +11,16 @@
  */
 class Solution {
 public:
+   
+    
+  
     TreeNode* invertTree(TreeNode* root) {
-          
-               if (root==NULL) return NULL;
-
-               swap(root->left, root->right);
-
-              invertTree(root->left);
-             invertTree(root->right);
-             return root;
-          
-    }
+                
+                if (!root) return nullptr;
+                 swap(root->left, root->right);
+                 invertTree(root->left);
+                 invertTree(root->right);
+            
+                   return root;
+    }  
 };
