14efbac4904bc87ea1ec038d1d6a30213a861685
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-11-18 09:45:43 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (41.51%) - LeetSync

---
+++ 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+          
+           int n = bits.size();
+           for(int i=0;i<n;i++){
+                 if (i==n-1){
+                     return bits[i]==0;
+                 }
+                 if (bits[i]==1){
+                      i++;
+                 }
+           }
+           return false;
+
+    }
+};
