335f12207f69f9c0703594adf9e90005e9f9dd2d
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2025-11-19 22:42:11 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (43.33%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,62 +1,35 @@
 class Solution {
 public:
-    bool dfs(int& u, int& v, unordered_map<int, vector<int>>& adj,
-             vector<bool>& visited) {
-                  
-                     visited[u]=1;
-                     if (u==v) return true;
-
-                     for(auto &check:adj[u]){
-                          if (visited[check]) continue;
-                          if (dfs(check,v,adj,visited)){
-                              return true;
-                          }
-                     }
-
-                     return false;
-
-
-             }
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-
-        int n = edges.size();
-        unordered_map<int, vector<int>> adj;
-        for (int i = 0; i < n; i++) {
-
-            int u = edges[i][0];
-            int v = edges[i][1];
-            vector<bool> visited(n, false);
-            if (adj.find(u) != adj.end() && adj.find(v) != adj.end() &&
-                dfs(u, v, adj, visited)) {
-                return edges[i];
-            }
-
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-        //  map<vector<int>,int>mp;
-        //  for (auto i:edges){
-        //      mp[i]++;
-        //  }
-        //    int n =edges.size();
-        //    int m=edges[0].size();
-        // for (int i=0;i<n;i++){
-
-        //      for (int j=i+1;j<n;j++){
-
-        //         if (edges[i][0] == edges[j][0]){
-        //               if (mp.count({edges[i][1],edges[j][1]})){
-        //                    return {edges[i][1],edges[j][1]};
-        //               }
-        //         }
-        //         else if (edges[i][1] == edges[j][0]){
-        //                if (mp.count({edges[i][0],edges[j][1]})){
-        //                    return {edges[i][0],edges[j][1]};
-        //               }
-        //         }
-
-        //      }
-        // }
-        return {-1,-1};
+    
+    int find(int i, vector<int>&parent){
+           if (parent[i]==i) return i;
+           return parent[i]= find(parent[i],parent);
     }
+    void unite(int i,int j,vector<int>&parent){
+           int lrep = find(i,parent);
+           int jrep = find(j,parent);
+           parent[lrep] = jrep;
+
+    }
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+                   int n = edges.size()+1;
+                   vector<int>parent(n+1,0);
+            for(int i=1;i<n;i++){
+                 parent[i]=i;
+            }
+               vector<int>ans;
+            for(auto i:edges){
+                  int u = i[0];
+                  int v = i[1];
+                  if (find(u,parent)!=find(v,parent)){
+                       unite(u,v,parent);
+                  }
+                  else{
+                      ans =i;
+                  }
+            }
+            return ans;
+
+    } 
 };
