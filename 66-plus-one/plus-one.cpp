2e7bcf088accfbac1bddd70dc06d4f88ee2fe7cc
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2026-01-01 10:04:08 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (50.17%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        
-        int n=digits.size();
-        for (int i=n-1;i>=0;i--){
-               
-               if (digits[i]<9){
-                        digits[i]+=1;
-                        return digits;
-               }
-               else{
-                     digits[i]=0;
-               }
-        }
-          digits.insert(digits.begin(), 1);
-        return digits;
+            int n = digits.size();
+            int carry=1;
+
+            for(int i=n-1;i>=0;i--){
+                   int sum = digits[i]+carry;
+                   if (sum>=10){
+                 
+                       carry=1;
+                   }else{
+                     carry =0;
+                   }
+                         digits[i]=sum%10;
+            }
+            if (carry==1){ 
+                  digits.insert(digits.begin(),1);
+            }
+            return digits;
     }
 };
