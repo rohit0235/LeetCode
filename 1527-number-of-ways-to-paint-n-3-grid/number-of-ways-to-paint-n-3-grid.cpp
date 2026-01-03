7d3a37629c08bb3d0608968c3a9fdc13c1b4008d
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2026-01-03 18:28:12 +0530

    Time: 20 ms (38.67%) | Memory: 9.4 MB (45.33%) - LeetSync

--- 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
@@ -1,60 +1,61 @@
 class Solution {
 public:
-           vector<string>cs;
-    int dp[5001][12];
-    void generate(  string curr, string prev, int m, int idx){
-          if(idx==m) {
+    vector<string> cs;
+    vector<vector<int>> adj;
+    int dp[5001][13];
+    const int mod = 1e9 + 7;
+
+    void generate(string curr, char prev, int n, int idx) {
+        if (idx == n) {
             cs.push_back(curr);
-            return ;
-          }
-        
-           for(auto i:{"R","G","B"}){
-                 if (i==prev ) continue;
-    
-                 generate(curr+i,i,m,idx+1 );
-           }
+            return;
+        }
 
+        for (char c : {'R', 'G', 'B'}) {
+            if (c == prev) continue;
+            generate(curr + c, c, n, idx + 1);
+        }
     }
-    const int mod = 1e9+7;
 
-    int solve(int rem, int previdx, int m){
-              
-              if (rem==0) return 1;
-              if (dp[rem][previdx]!=-1) return dp[rem][previdx];
-            string prev = cs[previdx];
-            int ways =0;
-              for(int i=0;i<cs.size();i++){
-                   if (i==previdx) continue;
-                   string curr = cs[i];
+    int solve(int rem, int prev) {
+        if (rem == 0) return 1;
 
-                   bool valid = true;
+        int &ans = dp[rem][prev];
+        if (ans != -1) return ans;
 
-                   for(int j=0;j<m;j++){
-                          if (prev[j]==curr[j]){
-                                  valid = false;
-                                  break;
-                          }
-                   }
-
-                   if (valid){
-                     ways= (ways + solve(rem-1,i,m))%mod;
-                   }
-              }
-              return dp[rem][previdx]=ways;   
+        ans = 0;
+        for (int nxt : adj[prev]) {
+            ans = (ans + solve(rem - 1, nxt)) % mod;
+        }
+        return ans;
     }
-    int numOfWays(int n)  {
-        // generate column states and for each column state check other 
-        int  m=3;
-        memset(dp,-1,sizeof(dp));
-          generate("","#",m,0);
- 
-          int k = cs.size();
-            int res=0;
-          for(int i=0;i<k;i++){
-               res= (res+ solve(n-1,i,m))%mod;
-          } 
 
-          return res;
-          
+    int numOfWays(int n) {
+        memset(dp, -1, sizeof(dp));
+
+        generate("", '#', 3, 0);   // column height fixed = 3
+        int k = cs.size();
+
+        // Precompute valid transitions
+        adj.assign(k, vector<int>());
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < k; j++) {
+                bool ok = true;
+                for (int r = 0; r < 3; r++) {
+                    if (cs[i][r] == cs[j][r]) {
+                        ok = false;
+                        break;
+                    }
+                }
+                if (ok) adj[i].push_back(j);
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < k; i++) {
+            res = (res + solve(n - 1, i)) % mod;
+        }
+
+        return res;
     }
 };
