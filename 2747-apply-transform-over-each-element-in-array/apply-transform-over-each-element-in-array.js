af73b5be3cbc92a96b2658add68b313c87f8ced3
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2026-01-10 10:19:05 +0530

    Time: 40 ms (80.03%) | Memory: 53.5 MB (81.54%) - LeetSync

---
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function (arr, fn) {
+
+   return arr.map(fn);
+
+};
+
+// var fn = function plusone(n) { return n + 1; }
