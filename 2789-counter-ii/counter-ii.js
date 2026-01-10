0a2ee2df13bb9f54bb5ee4ca30f9934168ce4976
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2026-01-10 10:08:47 +0530

    Time: 48 ms (74.17%) | Memory: 56.7 MB (41.50%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function (init) {
+    var res = init;
+    return {
+        increment: function () {
+               return init+=1;
+        },
+        decrement: function () {
+          return init-=1;
+        },
+        reset: function () {
+            init =res;
+            return res;
+        }
+
+
+    }
+
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
