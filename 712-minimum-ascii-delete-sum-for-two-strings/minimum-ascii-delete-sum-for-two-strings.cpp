8a3264aa7c490c61208478c9431fe52d8c3b7dc2
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2026-01-10 09:29:32 +0530

    Time: 47 ms (8.46%) | Memory: 13.5 MB (82.33%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,31 +1,46 @@
 class Solution {
 public:
-     int m,n;
-     
-     int solve(string &s1 , string& s2 , int i , int j,  vector<vector<int>>&dp){
-            if (i>=m && j>=n) return 0;\
-            if (dp[i][j]!=-1) return dp[i][j];
-            if (i>=m) {
-                 return s2[j]+solve(s1,s2,i,j+1,dp); 
-            }
-            if (j>=n) {
-                 return s1[i] +solve(s1,s2,i+1,j,dp);
-            }
-            if (s1[i]==s2[j]){
-                 return solve(s1,s2,i+1,j+1,dp);
-            }
+    
+    int n,m;
+    int t[1001][1001];
+ 
+    int solve(string& s1, string& s2, int i, int j){
+          if(i==n && j==m) return 0;
+           if(i>=n){
+               int sum =0;
+               while(j<m){
+                  sum+=s2[j];
+                  j++;
+               }
 
-            int d1=s1[i]+ solve(s1,s2, i+1 ,j,dp);
-            int d2=s2[j]+ solve(s1,s2, i ,j+1,dp);
-            
-            return dp[i][j]=min(d1,d2);
-     }
+               return sum;
+           }
+           if(j>=m){
+               int sum =0;
+               while(i<n){
+                  sum+=s1[i];
+                  i++;
+               }
 
+               return sum;
+           }
+           if(t[i][j]!=-1) return t[i][j];
+        //   int first =0;
+           if(s1[i]==s2[j]){
+             return solve(s1,s2,i+1,j+1);
+           }
+           int second = s1[i]+ solve(s1,s2,i+1,j);
+           int third =  s2[j]+solve(s1,s2,i,j+1);
+            // cout<<second<<" "<<third<<endl;
+          return t[i][j]=min(second,third);
+          
+    }
+  
     int minimumDeleteSum(string s1, string s2) {
-           
-         m= s1.size();       
-         n= s2.size();       
-         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-         return solve(s1,s2,0,0,dp);
+        n=s1.size();
+        m=s2.size();
+
+        memset(t,-1,sizeof(t));
+        return solve(s1,s2, 0,0);
     }
 };
