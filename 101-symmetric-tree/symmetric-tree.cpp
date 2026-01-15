7d4eeb8ba0b215ed1d6d3d34f032fcefa4ac029f
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2026-01-15 18:55:07 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (5.65%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -1,26 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(!p && !q) return true;
-        if(!p || !q) return false;
-        if(p->val!=q->val) return false;
-        return isSameTree(p->left, q->right) &&  isSameTree(p->right, q->left) ;
-
-
-    }
     bool isSymmetric(TreeNode* root) {
-        if(!root) return 1;
-        return isSameTree(root->left ,root->right) && isSameTree(root->right ,root->left) ;
+        if (!root) return true;
+        
+        queue<TreeNode*> q;
+        q.push(root->left);
+        q.push(root->right);
+        
+        while (!q.empty()) {
+            TreeNode* t1 = q.front();
+            q.pop();
+            TreeNode* t2 = q.front();
+            q.pop();
+            
+            if (!t1 && !t2) continue;
+            if (!t1 || !t2 || t1->val != t2->val) return false;
+            
+            q.push(t1->left);
+            q.push(t2->right);
+            q.push(t1->right);
+            q.push(t2->left);
+        }
+        
+        return true;
     }
 };
