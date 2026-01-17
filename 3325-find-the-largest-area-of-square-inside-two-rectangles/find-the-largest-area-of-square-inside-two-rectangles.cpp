d5daada64c3681ea660ae692dd6baa50652c0453
Rohit Kumar Sahu <rohitkumarsahu2022@vitbhopal.ac.in>
2026-01-17 12:44:03 +0530

    Time: 109 ms (45.46%) | Memory: 59.4 MB (80.17%) - LeetSync

--- 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.cpp
+++ 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.cpp
@@ -1,47 +1,21 @@
 class Solution {
 public:
-    long long largestSquareArea(vector<vector<int>>& bl,
-                                vector<vector<int>>& tr) {
-
-        int side = 0;
-
-        vector<vector<int>> h;
-        vector<vector<int>> v;
-        // vector<pair<int ,int> >v;
-
-        for (int i = 0; i < bl.size(); i++) {
-
-            h.push_back({bl[i][0], tr[i][0]});
-            v.push_back({bl[i][1], tr[i][1]});
-        }
-
-        for (int i = 0; i < bl.size() - 1; i++) {
-
-            // int maxh = h[i][0];
-            // int minh = h[i][1];
-            // int maxv = v[i][0];
-            // int minv = v[i][1];
-            //  cout<<maxh<<" "<<minh<<" ";
-            for (int j = i + 1; j < bl.size(); j++) {
-                int maxh = max(h[i][0], h[j][0]);
-                int minh = min(h[i][1], h[j][1]);
-
-                int maxv = max(v[i][0], v[j][0]);
-                int minv = min(v[i][1], v[j][1]);
-
-                //  cout<<maxh<<" "<<minh<<" "<<endl;;
-                //  cout<<maxv<<" "<<minv<<" "<<endl;;
-                //  cout<<endl;
-
-                int overlapX = minh - maxh; 
-                int overlapY = minv - maxv; 
-
-                int currSide = min(overlapX, overlapY);
-                if (currSide > 0)
-                    side = max(side, currSide);
+    long long largestSquareArea(vector<vector<int>>& bl, vector<vector<int>>& tr) {
+        long long ms = 0;
+        int n = bl.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                int x1 = max(bl[i][0], bl[j][0]);
+                int x2 = min(tr[i][0], tr[j][0]);
+                int y1 = max(bl[i][1], bl[j][1]);
+                int y2 = min(tr[i][1], tr[j][1]);
+                
+                if (x2 > x1 && y2 > y1) {
+                    long long s = min(x2 - x1, y2 - y1);
+                    ms = max(ms, s);
+                }
             }
         }
-
-        return 1LL * side * side;
+        return ms * ms;
     }
 };
